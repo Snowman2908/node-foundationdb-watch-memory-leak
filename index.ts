9d614cb910340e9e52e80e3b14ef34ce6e06e2bf
Snowman2908 <kirill2908@yandex.ru>
2024-05-21 20:03:16 +0700

    wip

--- index.ts
+++ index.ts
@@ -3,8 +3,10 @@ import bytes = require('bytes');
 import * as fs from 'fs';
 
 fdb.setAPIVersion(720);
+const db = fdb.open();
 
-main().catch((err) => console.log(err));
+startDoTransaction().catch((err) => console.log(err));
+startWatch().catch((err) => console.log(err));
 
 setInterval(() => {
     console.log('NodeJS heap:', bytes(process.memoryUsage().heapUsed));
@@ -18,12 +20,38 @@ setInterval(() => {
     console.log('Process memory:', bytes(bytes(vmRss)));
 }, 10_000);
 
-async function main() {
-    const db = fdb.open();
+async function startDoTransaction() {
+    for (let i = 0; i < 100; i++) {
+        const key = Buffer.from('key' + i);
 
+        doTransaction(key).catch((err) => console.log(err));
+    }
+}
+
+async function startWatch() {
+    for (let i = 0; i < 100; i++) {
+        const key = Buffer.from('key' + i);
+
+        watch(key).catch((err) => console.log(err));
+    }
+}
+
+async function watch(key: Buffer) {
     while (true) {
+        const watch = await db.doTn(async (tn) => {
+            return tn.watch(key);
+        });
+
+        await watch.promise;
+    }
+}
+
+async function doTransaction(key: Buffer) {
+    while (true) {
+        const randomValue = Buffer.from(Math.random().toString(36).substring(2, 15));
+
         await db.doTn(async (tn) => {
-            await tn.get('');
+            tn.set(key, randomValue);
         });
     }
 }
